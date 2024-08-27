3914ec490f73d8256571359fd7e316c4bd8685d2
Megan Duff <58375445+megan-duff@users.noreply.github.com>
2024-08-27 17:15:35 -0400

    Create outer_CEU_non_linear_pheno_sim.sh

---
+++ phenotype-data-simulation/additive-and-interaction-effects-phenotypes/outer_CEU_non_linear_pheno_sim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=CEU_non_linear_pheno_sim
+#SBATCH --output=CEU_non_linear_pheno_sim_%a.out
+#SBATCH --error=CEU_non_linear_pheno_sim_%a.err
+#SBATCH --partition=math-alderaan
+#SBATCH -N 1
+#SBATCH -n 64
+#SBATCH --array=1-110%10
+
+sim=${SLURM_ARRAY_TASK_ID}
+
+ancestry="CEU"
+
+cd /scratch/duffme_gensim/Simulations/${ancestry}/sim_${sim}/
+
+singularity exec /storage/singularity/mixtures.sif ~/Scripts/pheno_sim_scripts/target_non_linear_phenotype_generation.R "$ancestry" "$sim"
