249dfcc1068b8337eebf8ae6e8ce9bd86e9c151c
Megan Duff <58375445+megan-duff@users.noreply.github.com>
2024-09-04 14:44:59 -0400

    Create setup.py

---
+++ Softwares/genomic-cnn/setup.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='genomic-cnn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'pandas',
+        'pandas-plink',
+        'keras-tuner',
+        'dask',
+        'numpy',
+        'scikit-learn',
+        'matplotlib'
+    ],
+    entry_points={
+        'console_scripts': [
+            'genomic-cnn=main:main',
+        ],
+    },
+)
